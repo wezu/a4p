07a5e470141a9f5ca1159b571e38352381a200ad
wezu <grzechotnik1984@gmail.com>
2016-06-21 19:21:02 +0200

    missing files fix

--- shaders/compose_f.glsl
+++ shaders/compose_f.glsl
@@ -2,12 +2,26 @@
 #version 140
 #pragma include "inc_config.glsl"
 uniform sampler2D colorTex;
-uniform sampler2D blurTex;
+#if defined(ENABLE_BLUR) || defined(ENABLE_GLARE)|| defined(ENABLE_FLARE)|| defined(ENABLE_DISTORTION)
 uniform sampler2D auxTex;
+#endif
+#ifdef ENABLE_BLUR
+uniform sampler2D blurTex;
+uniform sampler2D dofTex;
+#endif
+
+#ifdef ENABLE_GLARE
 uniform sampler2D glareTex;
+#endif
+
+#ifdef ENABLE_FLARE
 uniform sampler2D flareTex;
 uniform sampler2D starTex;
+#endif
+
+#ifdef ENABLE_LUT
 uniform sampler2D lut;
+#endif
 
 in vec2 uv;
 in vec2 time_uv;
@@ -31,14 +45,26 @@ vec3 applyColorLUT(sampler2D lut, vec3 color)
 void main()
     {
     vec4 color=texture(colorTex,uv);
-    vec4 blured_color=texture(blurTex, uv);
+    #if defined(ENABLE_BLUR) || defined(ENABLE_GLARE)|| defined(ENABLE_FLARE)|| defined(ENABLE_DISTORTION)
     vec4 aux=texture(auxTex, uv);
+    #endif
 
+
+    #ifdef ENABLE_GLARE
     color+=texture(glareTex,uv);
-    color+=texture(flareTex,vec2(1.0, 1.0)-uv)*texture(starTex, rotate_uv);
-    color=mix(color, blured_color, aux.r);
+    #endif
 
-    #ifndef DISABLE_LUT
+    #ifdef ENABLE_FLARE
+    color+=texture(flareTex,vec2(1.0, 1.0)-uv)*texture(starTex, rotate_uv);
+    #endif
+
+    #ifdef ENABLE_BLUR
+    vec4 blured_color=texture(blurTex, uv);
+    float dof=texture(dofTex, rotate_uv).r;
+    color=mix(color, blured_color, aux.r*dof);
+    #endif
+
+    #ifdef ENABLE_LUT
     color.rgb=applyColorLUT(lut, color.rgb);
     #endif
 
